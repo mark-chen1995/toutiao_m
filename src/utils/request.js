e27b614784b9301fe6296a5d86419b74ef956576
markchen <1975016204@qq.com>
2022-07-04 17:36:02 +0800

    添加响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,5 +3,14 @@ import axios from 'axios'
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/'
 })
-
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  const { data } = response
+  return data
+}, function (error) {
+  // 对响应错误做点什么
+  console.log(error.response)
+  return Promise.reject(error.response)
+})
 export default request
